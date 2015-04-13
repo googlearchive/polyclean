8564605b4dd43a013bad90e126e8cc9413fb4d16
AJ Ortega <ajo@google.com>
2015-04-12 23:21:25 -0700

    Add uglify.

--- index.js
+++ index.js
@@ -7,32 +7,55 @@
  * Code distributed by Google as part of the polymer project is also
  * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
  */
+var dom5 = require('dom5')
 var escodegen = require('escodegen');
 var espree = require('espree');
 var through2 = require('through2');
-var dom5 = require('dom5')
+var uglify = require('uglify-js');
+
+/**
+ * Transforms all inline scripts in `html` with `filter`.
+ */
+function filterInlineScript(html, filter) {
+  var p = dom5.predicates;
+  var parsedHtml = dom5.parse(html);
+  var isInlineScript = p.AND(
+    p.hasTagName('script'),
+    p.NOT(p.hasAttr('src'))
+  );
+  dom5.queryAll(parsedHtml, isInlineScript)
+    .forEach(function(inlineScript) {
+      var text = dom5.getTextContent(inlineScript);
+      dom5.setTextContent(inlineScript,filter(text));
+    });
+  return dom5.serialize(parsedHtml);
+}
 
 var exports = {
   /**
    * Gulp plugin that removes comments with a parser roundtrip.
    */
   cleanJsComments: function cleanJsComments() {
-    var p = dom5.predicates;
     return through2.obj(function(file, encoding, cb) {
-      var parsedHtml = dom5.parse(String(file.contents));
-      var isInlineScript = p.AND(
-        p.hasTagName('script'),
-        p.NOT(p.hasAttr('src'))
-      );
-      dom5.queryAll(parsedHtml, isInlineScript)
-        .forEach(function(inlineScript) {
-          var text = dom5.getTextContent(inlineScript);
+      var cleaned = filterInlineScript(String(file.contents), function(text) {
           content = espree.parse(text, {attachComment: false, comments: false});
-          dom5.setTextContent(inlineScript,
-            escodegen.generate(content)
-            );
-        });
-      file.contents = new Buffer(dom5.serialize(parsedHtml));
+          return escodegen.generate(content);
+      });
+      file.contents = new Buffer(cleaned);
+      this.push(file);
+      return cb();
+    });
+  },
+
+  /**
+   * Uglify the stream.
+   */
+  uglifyJs: function uglifyJs() {
+    return through2.obj(function(file, encoding, cb) {
+      var cleaned = filterInlineScript(String(file.contents), function(text) {
+          return uglify.minify(text, {fromString: true}).code;
+      });
+      file.contents = new Buffer(cleaned);
       this.push(file);
       return cb();
     });
